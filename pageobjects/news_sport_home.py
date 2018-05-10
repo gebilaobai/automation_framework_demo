5778a34c48d273cdbf816044640a4bb626fe43c5
baiy <baiy@glodon.com>
2018-05-10 11:08:09 +0800

    first commit

---
+++ pageobjects/news_sport_home.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+
+from framework.base_page import BasePage
+
+
+class SportNewsHomePage(BasePage):
+    # NBA入口
+    nba_link = "xpath=>//*[@id='col_nba']/div[1]/div[2]/ul[1]/li[1]/a"
+
+    def click_nba_link(self):
+        self.click(self.nba_link)
+        self.sleep(2)
